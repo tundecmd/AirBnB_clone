8b3dbb000e906a46be3fa5acedd904e40dd35ac2
Adepoju Lateef <ademustexcel@gmail.com>
2023-02-05 16:37:48 +0100

    attempted task 3

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """ A BaseModel class that defines all common attributes/methods for other classes """
+    def __init__(self):
+        """ Initializes the class """
+        print("init function")
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    def __str__(self):
+        """ A string representation that is readable to humans """
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+
+    def save(self):
+        """ updates the public instance attribute updated_at with the current datetime """
+        self.updated_at = datetime.now()
+        print("save")
+
+    def to_dict(self):
+        """  Returns a dictionary containing all keys/values of __dict__ of the instance """
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+
+        dates_to_be_converted = ("created_at", "updated_at")
+        for k, v in self.__dict__.items():
+            if k in dates_to_be_converted:
+                my_dict[k] = my_dict[k].isoformat()
+        # Basically, this is what we did above
+        # my_dict["created_at"] = my_dict["created_at"].isoformat() 
+        # my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+       
+        return my_dict
