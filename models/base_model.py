0f853aac4b2db6c41026f8bdfa6a524fab3d9ab1
Adepoju Lateef <ademustexcel@gmail.com>
2023-02-06 12:26:02 +0100

    added shebang line to line 1 of base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,32 @@
-import uuid
+#!/usr/bin/env python3
+from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
     """ A BaseModel class that defines all common attributes/methods for other classes """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initializes the class """
         print("init function")
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    
+            
+
     
     def __str__(self):
-        """ A string representation that is readable to humans """
+        """ A string representation that is readablinfe to humans """
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-
-
+        
     def save(self):
-        """ updates the public instance attribute updated_at with the current datetime """
+        """ updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
         print("save")
+        return
 
     def to_dict(self):
         """  Returns a dictionary containing all keys/values of __dict__ of the instance """
